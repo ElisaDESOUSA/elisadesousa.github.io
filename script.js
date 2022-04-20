c1d672e724563f1aa51c4501061346354f36cf4d
Elisa <92156336+ElisaDESOUSA@users.noreply.github.com>
2022-04-20 16:30:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var myText = "développeuse";
+
+var myArray = myText.split(""); 
+
+var timeLooper;
+
+function loop(){
+    if(myArray.length > 0) {
+        document.getElementById('typewriter').innerHTML += myArray.shift();
+    } else {
+        clearTimeout(timeLooper);
+    }
+    timeLooper = setTimeout('loop()', 70);
+}
+
+loop()
